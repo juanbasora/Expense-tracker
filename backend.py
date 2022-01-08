d61651958b28a6b6ea26d6ec5657d546d2307ac9
juanbasora <juanbasora89@gmail.com>
2022-01-07 19:45:42 -0500

    Changed some functions on the backend.

--- backend.py
+++ backend.py
@@ -5,11 +5,11 @@ class Database:
 	def __init__(self):
 		self.conn = sqlite3.connect("expenses.db")
 		self.cur = self.conn.cursor()
-		self.cur.execute("CREATE TABLE IF NOT EXISTS expenses (date text, amount integer, category text, notes text)")
+		self.cur.execute("CREATE TABLE IF NOT EXISTS expenses (day date, amount integer, category text, notes text)")
 		self.conn.commit()
 
-	def add_entry(self, date, amount, category, notes):
-		self.cur.execute("INSERT INTO expenses VALUES (?, ?, ?, ?)", (date, amount, category, notes))
+	def add_entry(self, day, amount, category, notes):
+		self.cur.execute("INSERT INTO expenses VALUES (?, ?, ?, ?)", (day, amount, category, notes))
 		self.conn.commit()
 
 	def view_all(self):
@@ -17,14 +17,13 @@ class Database:
 		rows = self.cur.fetchall()
 		return rows
 
-	def search(self, title=""):
-		self.cur.execute("SELECT * FROM expenses WHERE title=? OR author=? OR year=? OR isbn=?", (date, amount, category, notes))
+	def search(self, day="", amount="", category="", notes=""):
+		self.cur.execute("SELECT * FROM expenses WHERE day=? OR amount=? OR category=? OR notes=?", (day, amount, category, notes))
 		rows = self.cur.fetchall()
 		return rows
 
 	def delete(self, id):
-		self.cur.execute("DELETE FROM expenses WHERE id=?", (id,))
-		self.conn.commit()
+		pass
 
 	def __del__(self):
 		self.conn.close()
